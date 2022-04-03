d2729cd36bc46ac36f0234424a1e2eae6e605c47
iris <iris>
2022-04-03 11:57:25 +0200

    Creado Caso Practico CRUD - Hospital.

---
+++ CRUD-Hospital/src/Hospital/Hospital.java
@@ -0,0 +1,55 @@
+package Hospital;
+
+public class Hospital {
+    
+    private final int id;
+    private String nombre;
+    private String direccion;
+    private String telefono;
+    private String localidad;
+
+    public Hospital(int id, String nombre, String direccion, String telefono, String localidad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.localidad = localidad;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getLocalidad() {
+        return localidad;
+    }
+
+    public void setLocalidad(String localidad) {
+        this.localidad = localidad;
+    }
+    
+}
---
+++ CRUD-Hospital/src/Hospital/Main.java
@@ -0,0 +1,20 @@
+package Hospital;
+
+public class Main {
+    
+    public static void menu(){
+        
+        System.out.println("1. Cambiar nombre"
+                + "\n2. Cambiar direccion"
+                + "\n3. Cambiar telefono"
+                + "\n4. Cambiar localidad"
+                + "\n5. Añadir hospital"
+                + "\n6. Borrar hospital"
+                + "\n7. Buscar hospital");
+    }
+    
+    public static void main(String[] args) {
+        
+    }
+    
+}
