1f2cd7c937c31d45abf410a5a0e7c0d56d4eb225
josedavid-celsor <josedavidrosalesrios0310@gmail.com>
2022-04-05 17:06:39 +0200

    Creado Clase Doctor y DBDoctor(actualizado)

--- CRUD-Hospital/src/Hospital/DBDoctor.java
+++ CRUD-Hospital/src/Hospital/DBDoctor.java
@@ -13,7 +13,7 @@ public class DBDoctor {
         connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
     }
 
-    public void añadirHospital(Doctor d) throws SQLException {
+    public void añadirDoctor(Doctor d) throws SQLException {
         String sql = "INSERT INTO doctores (nombre, direccion, localidad, telefono) VALUES ('" + d.getNombre() + "', '" + d.getTelefono() + "','" + d.getId_hospital()+ "','" + d.getEspecialidad() + "','" + d.getFechaNac() + "')";
 
         Statement stm = connection.createStatement();
@@ -23,7 +23,7 @@ public class DBDoctor {
         connection.close();
     }
 
-    public void eliminarHospital(Doctor d) throws SQLException {
+    public void eliminarDoctor(Doctor d) throws SQLException {
         String sql = "DELETE FROM doctores WHERE id=" + d.getId();
 
         Statement stm = connection.createStatement();
@@ -53,8 +53,8 @@ public class DBDoctor {
         connection.close();
     }
 
-    public void actualizarDireccionHospital(Hospital h) throws SQLException {
-        String sql = "UPDATE hospitales SET direccion='" + h.getDireccion() + "'" + "WHERE id=" + h.getId();
+    public void actualizarTelefonoDoctor(Doctor d) throws SQLException {
+        String sql = "UPDATE doctores SET telefono='" + d.getTelefono()+ "'" + "WHERE id=" + d.getId();
 
         Statement stm = connection.createStatement();
         stm.execute(sql);
@@ -63,18 +63,8 @@ public class DBDoctor {
         connection.close();
     }
 
-    public void actualizarLocalidadHospital(Hospital h) throws SQLException {
-        String sql = "UPDATE hospitales SET localidad='" + h.getLocalidad() + "'" + "WHERE id=" + h.getId();
-
-        Statement stm = connection.createStatement();
-        stm.execute(sql);
-
-        stm.close();
-        connection.close();
-    }
-
-    public void actualizarTelefonoHospital(Hospital h) throws SQLException {
-        String sql = "UPDATE hospitales SET telefono='" + h.getTelefono() + "'" + "WHERE id=" + h.getId();
+    public void actualizarEspecialidad(Doctor d) throws SQLException {
+        String sql = "UPDATE doctores SET especialidad='" + d.getEspecialidad() + "'" + "WHERE id=" + d.getId();
 
         Statement stm = connection.createStatement();
         stm.execute(sql);
@@ -84,17 +74,18 @@ public class DBDoctor {
     }
 
     public void obtener() throws SQLException {
-        String sql = "SELECT * FROM hospitales";
+        String sql = "SELECT * FROM doctores";
         
         Statement stm = connection.createStatement();
         ResultSet rs = stm.executeQuery(sql);
         while (rs.next()) {
             int id = rs.getInt("id");
             String nombre = rs.getString("nombre");
-            String direccion = rs.getString("direccion");
-            String localidad = rs.getString("localidad");
+            String id_hospital = rs.getString("id_hospital");
+            String especialidad = rs.getString("especialidad");
             String telefono = rs.getString("telefono");
-            System.out.println(String.format("%d. %s - %s - %s - %s", id, nombre, direccion, localidad, telefono));
+            Date fechaNac = rs.getDate("fechaNac"); 
+            System.out.println(String.format("%d. %s - %s - %s - %s - %s", id, nombre, id_hospital, especialidad, telefono, fechaNac));
         }
         stm.close();
         rs.close();
